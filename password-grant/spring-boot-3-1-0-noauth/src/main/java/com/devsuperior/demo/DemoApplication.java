f37b8658f76f44c6a1d7b781f2cb1fab7bace580
Luiz Carlos <luizzlcs@gmail.com>
2025-01-09 16:07:07 -0300

    BCryp password

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,13 +1,29 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner{
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		// Testando o PasswordEncoder
+
+		String password = "1234567";
+		System.out.println("ENCODER: " + passwordEncoder.encode(password));
+		System.out.println("COMPARE: " + passwordEncoder.matches(password, "$2a$10$cCdAJTvnM7vJLe7RVKsH0eDsUIKE3jaCC3Uyp6x523IumnvZDEsjC"));
 	}
 
 }
--- password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$cCdAJTvnM7vJLe7RVKsH0eDsUIKE3jaCC3Uyp6x523IumnvZDEsjC');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$cCdAJTvnM7vJLe7RVKsH0eDsUIKE3jaCC3Uyp6x523IumnvZDEsjC');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
